764242c6d776eac0f60d59604bb0646f079dc8ac
Thanh Tùng <electricthanhtung@gmail.com>
2024-03-30 20:54:11 +0700

    Add define to get leng of array in C/C++

--- MJVM/Inc/mjvm_common.h
+++ MJVM/Inc/mjvm_common.h
@@ -4,6 +4,8 @@
 
 #include <stdint.h>
 
+#define LENGTH(array)           (sizeof(array) / sizeof(array[0]))
+
 uint16_t swap16(uint16_t value);
 uint32_t swap32(uint32_t value);
 uint64_t swap64(uint64_t value);
