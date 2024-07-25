2c5eb28b899f5d8f75fe17577edcfed34dbf001e
Thanh Tùng <electricthanhtung@gmail.com>
2024-07-25 20:11:30 +0700

    Split MSDK and MjvmDebug into separate repositories

--- MSDK/Src/module-info.java
+++
@@ -1,9 +0,0 @@
-
-module java.base {
-    exports java.io;
-    exports java.lang;
-    exports java.math;
-    exports java.util;
-    exports jdk.internal.math;
-    exports java.lang.annotation;
-}
