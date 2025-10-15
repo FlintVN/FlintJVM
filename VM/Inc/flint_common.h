0d899c3e48e0e6caf3450456c2a695e2ecead7b7
Thanh Tùng <electricthanhtung@gmail.com>
2025-10-15 12:04:37 +0700

    Update version number

--- VM/Inc/flint_common.h
+++ VM/Inc/flint_common.h
@@ -6,7 +6,7 @@
 
 #define FLINT_VERSION_MAJOR         2
 #define FLINT_VERSION_MINOR         1
-#define FLINT_VERSION_PATCH         0
+#define FLINT_VERSION_PATCH         1
 
 #define LENGTH(_array)              (sizeof(_array) / sizeof(_array[0]))
 
